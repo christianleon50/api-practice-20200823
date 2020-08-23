7167bb50b7f7a2bc23c13ae3bfc70fcbb71e28d7
christianleon50 <christian.leon@misena.edu.co>
2020-08-23 11:51:51 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -23,19 +23,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::resource('Worker', 'WorkerController');
 
 Route::get('Labor_profile','Labor_profileController@index')->name('laborp.index');
-Route::get('Labor_profile','Labor_profileController@store')->name('laborp.store');
+Route::post('Labor_profile','Labor_profileController@store')->name('laborp.store');
 Route::get('Labor_profile','Labor_profileController@show')->name('laborp.show');
-Route::get('Labor_profile','Labor_profileController@update')->name('laborp.update');
-Route::get('Labor_profile','Labor_profileController@destroy')->name('laborp.destroy');
+Route::put('Labor_profile','Labor_profileController@update')->name('laborp.update');
+Route::delete('Labor_profile','Labor_profileController@destroy')->name('laborp.destroy');
 
 Route::get('Work_experience','Work_experienceController@index')->name('workxp.index');
-Route::get('Work_experience','Work_experienceController@store')->name('workxp.store');
+Route::post('Work_experience','Work_experienceController@store')->name('workxp.store');
 Route::get('Work_experience','Work_experienceController@show')->name('workxp.show');
-Route::get('Work_experience','Work_experienceController@update')->name('workxp.update');
-Route::get('Work_experience','Work_experienceController@destroy')->name('workxp.destroy');
+Route::put('Work_experience','Work_experienceController@update')->name('workxp.update');
+Route::delete('Work_experience','Work_experienceController@destroy')->name('workxp.destroy');
 
 Route::get('Worker','WorkerController@index')->name('worker.index');
-Route::get('Worker','WorkerController@store')->name('worker.store');
+Route::post('Worker','WorkerController@store')->name('worker.store');
 Route::get('Worker','WorkerController@show')->name('worker.show');
-Route::get('Worker','WorkerController@index')->name('worker.update');
-Route::get('Worker','WorkerController@index')->name('worker.destroy');
+Route::put('Worker','WorkerController@update')->name('worker.update');
+Route::delete('Worker','WorkerController@destroy')->name('worker.destroy');
